a94bd9a069d864da6907d1d2e36ab6a2d19e2723
manzurahammed <manzur.ahammed236@gmail.com>
2018-07-21 16:29:49 +0600

    init

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import bookReducer from './reducer_books';
+
+const rootReducer = combineReducers({
+  books:bookReducer,
+});
+
+export default rootReducer;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {title:'LOL 1'},
+        {title:'LOL 2'},
+        {title:'LOL 3'},
+        {title:'LOL 4'}
+    ]
+}
